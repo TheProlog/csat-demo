5282c441c34b14e64211680613861dd20848fcc6
Jeff Dickey <jdickey@seven-sigma.com>
2014-01-22 00:03:37 +0800

    Feature spec `belt_and_suspenders` working again. Automation FTW!
    This is specifically important for testing the “welcome” page and its execute-on-document-ready scripting.
    
    RSpec status:	 		11 examples, 0 failures
    Teaspoon status:	 	253 tests: 0 failures, 0 errors, 78 pending, 0 omitted
    Custom RuboCop rules:	None
    Inline RuboCop disabling:	None (in non-generated code)
    
    Trello cards authorising work:
    * https://trello.com/c/2uslRmdQ/27-clean-up-refactor-welcome-js-coffee-to-get-all-the-procedural-crap-h-h-h-h-h-support-code-into-other-files
    * https://trello.com/c/h6Kw6FkX/14-main-app-landing-page (partial)

--- spec/features/belt_and_suspenders_spec.rb
+++ spec/features/belt_and_suspenders_spec.rb
@@ -8,26 +8,33 @@ describe 'User visits landing page and', js: true do
     visit root_path
   end
 
-  xit 'submits the form' do
+  it 'submits the form' do
     within '#start_panel' do
-      select 'p:nth-child(2)', from: 'Selector'
+      select 'h1:nth-child(1)', from: 'Selector'
       fill_in 'start-nodeindex', with: '0'
-      fill_in 'start-textoffset', with: '2'
+      fill_in 'start-textoffset', with: '5'
     end
     within '#end_panel' do
-      select 'p:nth-child(2)', from: 'Selector'
-      fill_in 'end-nodeindex', with: '2'
-      fill_in 'end-textoffset', with: '4'
+      select 'p:nth-child(2) > strong:nth-child(2)', from: 'Selector'
+      fill_in 'end-nodeindex', with: '0'
+      fill_in 'end-textoffset', with: '12'
     end
     click_button 'Select'
-    expected = 'Selection has the base selector .boilerplate.' +
-        ' The starting endpoint has the selector p:nth-child(2),' +
-        ' the node index 0, ' +
-        ' and the text offset 2.' +
-        'The ending endpoint has the selector p:nth-child(2),' +
-        ' the node index 2,' +
-        ' and the text offset 4.'
-    expect(page).to have_content expected
+    expect(page.find '#alertbox').to have_content expected_content
   end
 
+end
+
+# Why have this return a Regexp? Capybara and/or selenium_webdriver make
+# 'interesting' changes to the markup; for example, putting various spurious
+# attributes in the `<strong>` tag, and one or more spaces after the `</h1>`
+# and the opening `<p>`.
+def expected_content
+  text = '<h1>is the most important headline</h1>\s*?' +
+    '<p>\s*?' +
+    'This is ordinary paragraph text within the body of the document,' +
+    ' where certain words and phrases may be ' +
+    '<em>emphasized</em> to mark them as ' +
+    '<strong.*>particularly</strong>'
+  Regexp.new text
 end
